e8a5cf5a13e4859e233df457c9a2d4e76c3d9a48
David Ritchey <ritchey0713@gmail.com>
2019-03-22 21:51:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,32 +1,34 @@
 require 'pry'
-# def oxford_comma(array)
-#   if array.length == 1 
-#     return array.join 
+
+def oxford_comma(array)
+  if array.length == 1 
+    return array.join 
     
-#   elsif array.length == 2 
-#     return array.join(" and ")
+  elsif array.length == 2 
+    return array.join(" and ")
     
-#   elsif array.length >= 3
-#     array[-1] = "and #{array[-1]}"
-#     return array.join(", ")
-#   end 
-# end
+  elsif array.length >= 3
+  binding.pry 
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end 
+end
 
 # .join doesnt work array > 2 
 
-def oxford_comma(array)
-  binding.pry 
-  case array 
+# def oxford_comma(array)
+#   binding.pry 
+#   case array 
   
-  when array.length == 1 
-    return array.join 
+#   when array.length == 1 
+#     return array.join 
     
-  when array.length == 2 
-    return array.join(" and ")
+#   when array.length == 2 
+#     return array.join(" and ")
     
-  when array.length >= 3
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+#   when array.length >= 3
+#     array[-1] = "and #{array[-1]}"
+#     return array.join(", ")
     
-  end 
-end
+#   end 
+# end
