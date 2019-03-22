23cfd0b11e1b85b7ab59d0e8eac176160c147545
David Ritchey <ritchey0713@gmail.com>
2019-03-22 21:42:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,26 @@
-def oxford_comma(array)
-  if array.length == 1 
-    return array.join 
+# def oxford_comma(array)
+#   if array.length == 1 
+#     return array.join 
     
-  elsif array.length == 2 
+#   elsif array.length == 2 
+#     return array.join(" and ")
+    
+#   elsif array.length >= 3
+#     array[-1] = "and #{array[-1]}"
+#     return array.join(", ")
+#   end 
+# end
+
+def oxford_comma(array)
+  case array 
+  
+  when array.length == 1 
+  
+    return array.join 
+  when array.length == 2 
     return array.join(" and ")
     
-  elsif array.length >= 3
+  when  array.length >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end 
