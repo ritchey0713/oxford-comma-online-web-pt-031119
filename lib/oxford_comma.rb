de3867abe26784ea6f8dbe16ff6f9eac6b19ac0e
David Ritchey <ritchey0713@gmail.com>
2019-03-22 21:38:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     return array.join(" and ")
     
-  elsif array.length == 3
+  elsif array.length >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end 
