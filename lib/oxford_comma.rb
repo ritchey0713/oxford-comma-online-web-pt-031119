367d548c1aeba54fc1ff011eb6f4efb01f798f69
David Ritchey <ritchey0713@gmail.com>
2019-03-22 21:42:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -15,13 +15,14 @@ def oxford_comma(array)
   case array 
   
   when array.length == 1 
-  
     return array.join 
+    
   when array.length == 2 
     return array.join(" and ")
     
   when  array.length >= 3
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
+    
   end 
 end
